1fc96a54db502a760c436ac8e91469e5e6b2259a
Joao <jpls@cin.ufpe.br>
2024-11-29 00:09:35 -0300

    use environment variables to config project

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,29 @@ import { TransactionsModule } from './transactions/transactions.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { File, Transactions } from './transactions/entities';
 import { FileManageModule } from './fileManage/fileManage.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TransactionsModule,
-    TypeOrmModule.forRoot({
-      entities: [File, Transactions],
-      host: 'mysqlDB',
-      username: 'root',
-      password: 'root',
-      database: 'mysqlDB',
-      retryAttempts: 10,
-      retryDelay: 5000,
-      type: 'mysql',
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        entities: [File, Transactions],
+        host: configService.get<string>('DATABASE_HOST') || 'mysqlDB',
+        username: configService.get<string>('DATABASE_USER') || 'root',
+        password: configService.get<string>('DATABASE_PASS') || 'root',
+        database: configService.get<string>('DATABASE_NAME') || 'mysqlDB',
+        retryAttempts: 10,
+        retryDelay: 5000,
+        type: 'mysql',
+        synchronize: true,
+      }),
     }),
     FileManageModule,
   ],
